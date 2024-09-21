31dc292ecbdcea6fa2335b79a81d85a645811dc7
Arun Jangra <arunjangra1001@gmail.com>
2024-09-21 08:57:46 +0800

    feat : updated somethings

--- da-service/src/constants.ts
+++ da-service/src/constants.ts
@@ -1,6 +1,7 @@
 export const PRIV_KEY = "";
-export const PUBLIC_KEY = "";
-export const AVAIL_ENDPOINT = "";
-export const AVAIL_SEED = "";
-export const AVAIL_ID = "";
+export const PUBLIC_KEY = "5EcPiksgkARM1c5YxnZ6uuHHSnZ2VEBFqkY8tktGK1jV7cut";
+export const AVAIL_ENDPOINT = "https://turing-rpc.avail.so/rpc";
+export const AVAIL_SEED =
+  "river own angry plate double sheriff humble diamond gun crazy answer swarm";
+export const AVAIL_ID = ""; // AVAIL RPC DOWN
 export const PORT = "3000";
