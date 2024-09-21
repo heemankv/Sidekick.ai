fccc6a419a9bdcadb59cc90fb892027c4a8e6e6f
anukkrit149 <anukkrit.official@gmail.com>
2024-09-21 20:06:43 +0800

    [] integration api server

---
+++ llm/structs.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from pydantic.types import UUID
+from typing import Dict, Optional
+
+
+class CreateUserRequest(BaseModel):
+    world_coin_id: str
+
+
+class BaseLLMInputData(BaseModel):
+    user_id: UUID
+    prompt_type: str
+    prompt: str
+
+
+class UserModel(BaseModel):
+    user_id: str
+    world_coin_id: str
+    value: Optional[Dict] = None
